f1d6c19b18f00b3a2ac6d963cbe5b3ea57e393db
philippe <pythonjsdev@gmail.com>
2022-12-30 21:16:11 +0100

    suppression null=true dans les champs string + maj dockerfile(comment),start et entrypoint

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -26,18 +26,15 @@ class Project(models.Model):
         Category, related_name='projects', on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     slug = models.SlugField(blank=True)
-    items_list = models.CharField(max_length=2000, blank=True, null=True)
+    items_list = models.CharField(max_length=2000, blank=True)
     description = models.TextField(blank=True)
     short_description = models.CharField(max_length=256, blank=True)
     keywords_list = models.CharField(max_length=256, blank=True)
     url_github_oc = models.URLField(max_length=200, blank=True, null=True)
-    url_github_portfolio = models.URLField(
-        max_length=200, blank=True, null=True)
+    url_github_portfolio = models.URLField(max_length=200, blank=True, null=True)
     thumbnail = models.ImageField(upload_to='projects', blank=True, null=True)
-    related_project_name = models.CharField(
-        max_length=128, blank=True, null=True)
-    related_project_url = models.URLField(
-        max_length=200, blank=True, null=True)
+    related_project_name = models.CharField(max_length=128, blank=True)
+    related_project_url = models.URLField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.name
